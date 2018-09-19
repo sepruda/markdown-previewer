5b8457c2e921075ab251007880d313aa5ed82cd8
Christian Jess Mark <christianjessmark@gmail.com>
2018-09-19 11:58:30 +0200

    style box-shadow

--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
-// TODO:
-//     1. Fix Box-Shadow small devices
